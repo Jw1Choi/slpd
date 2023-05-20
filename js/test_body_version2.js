9997b0841145b9ae9f7e63c8b9bef9bbc0416add
Choijiwoong <86772852+Jw1Choi@users.noreply.github.com>
2023-05-21 02:37:23 +0900

    Add files via upload

--- js/test_body_version2.js
+++ js/test_body_version2.js
@@ -165,10 +165,10 @@ testbody.innerHTML =`
 			
 
 							 <section  id="main_intro" class="backg" style="text-align: center;" >
-							 <video playsinline autoplay loop muted id="myVideo pv" style="position:fixed;top:0;left:0;right:0;min-width: 100%; min-height: 100%;z-index:0;background-size:cover;filter:brightness(0.5)" >
+							 <video playsinline autoplay loop muted id="myVideo pv" style="position:fixed;top:0;left:0;right:0;min-width: 100%; min-height: 100%;z-index:5;background-size:cover;filter:brightness(0.5)" >
 							 <source src="img/intro_vid1.mp4" type="video/mp4" >
 							 </video>
-							 <div class="img-text md-top3 lg-top3 top-space " style="font-size:14px;">
+							 <div class="img-text md-top3 lg-top3 top-space " style="font-size:14px;z-index:10;">
 					
  
 							 <div class="container">
